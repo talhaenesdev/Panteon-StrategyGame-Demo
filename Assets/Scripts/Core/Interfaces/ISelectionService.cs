dfeef2f50fe96aadf8294346484984f8a60c00db
Talha Enes İSLAMOĞLU <100480785+HorseMan1996@users.noreply.github.com>
2026-07-29 15:03:42 +0300

    selection service added

---
+++ Assets/Scripts/Core/Interfaces/ISelectionService.cs
@@ -0,0 +1,13 @@
+﻿using PanteonStrategyGame.Units.Models;
+
+namespace PanteonStrategyGame.Core.Interfaces
+{
+    public interface ISelectionService
+    {
+        Unit SelectedUnit { get; }
+
+        void Select(Unit unit);
+
+        void ClearSelection();
+    }
+}
