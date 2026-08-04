e325ff5a825f834fa0f44b52fcd76c27a1bfa0ad
Talha Enes İSLAMOĞLU <100480785+HorseMan1996@users.noreply.github.com>
2026-08-04 19:04:33 +0300

    Factory build added

--- Assets/Scripts/Buildings/Data/BuildingType.cs
+++ Assets/Scripts/Buildings/Data/BuildingType.cs
@@ -3,6 +3,7 @@
     public enum BuildingType
     {
         Barracks,
-        PowerPlant
+        PowerPlant,
+        Factory
     }
 }
---
+++ Assets/Scripts/Buildings/Models/GeneralBuilding.cs
@@ -0,0 +1,11 @@
+﻿using PanteonStrategyGame.Buildings.Models;
+using UnityEngine;
+
+namespace Assets.Scripts.Buildings.Models
+{
+    internal class GeneralBuilding : Building
+    {
+        public override string DisplayName => buildingData.DisplayName;
+        public override Sprite Icon => buildingData.Icon;
+    }
+}
